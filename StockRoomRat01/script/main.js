11e9069f2f3ca7004970a358fc161de63e38b576
stckTm <13stck@gmail.com>
2025-08-17 02:07:42 +0900

    adding some scripts...

--- StockRoomRat01/script/main.js
+++ StockRoomRat01/script/main.js
@@ -1,74 +1,86 @@
 // adobe font
-(function (d) {
-    var config = {
-            kitId: "bpb3ikw",
-            scriptTimeout: 3000,
-            async: true,
-        },
-        h = d.documentElement,
-        t = setTimeout(function () {
-            h.className =
-                h.className.replace(/\bwf-loading\b/g, "") + " wf-inactive";
-        }, config.scriptTimeout),
-        tk = d.createElement("script"),
-        f = false,
-        s = d.getElementsByTagName("script")[0],
-        a;
-    h.className += " wf-loading";
-    tk.src = "https://use.typekit.net/" + config.kitId + ".js";
-    tk.async = true;
-    tk.onload = tk.onreadystatechange = function () {
-        a = this.readyState;
-        if (f || (a && a != "complete" && a != "loaded")) return;
-        f = true;
-        clearTimeout(t);
-        try {
-            Typekit.load(config);
-        } catch (e) {}
-    };
-    s.parentNode.insertBefore(tk, s);
-})(document);
+;(function (d) {
+  var config = {
+      kitId: "bpb3ikw",
+      scriptTimeout: 3000,
+      async: true,
+    },
+    h = d.documentElement,
+    t = setTimeout(function () {
+      h.className = h.className.replace(/\bwf-loading\b/g, "") + " wf-inactive"
+    }, config.scriptTimeout),
+    tk = d.createElement("script"),
+    f = false,
+    s = d.getElementsByTagName("script")[0],
+    a
+  h.className += " wf-loading"
+  tk.src = "https://use.typekit.net/" + config.kitId + ".js"
+  tk.async = true
+  tk.onload = tk.onreadystatechange = function () {
+    a = this.readyState
+    if (f || (a && a != "complete" && a != "loaded")) return
+    f = true
+    clearTimeout(t)
+    try {
+      Typekit.load(config)
+    } catch (e) {}
+  }
+  s.parentNode.insertBefore(tk, s)
+})(document)
 
 //hero-animation
 document.addEventListener("DOMContentLoaded", () => {
-    const heroAnimate = gsap.timeline();
-    gsap.from(".lamp__line", {
-        opacity: 0,
-        duration: 1,
-        delay: 0,
-        height: 0,
-    });
-    heroAnimate.from(".lamp__triangle", {
-        opacity: 0,
-        duration: 0.6,
-        delay: 0.5,
-        height: 0,
-        width: 0,
-    });
-    heroAnimate.from(".lamp__light", {
-        opacity: 0,
-        duration: 0.3,
-        delay: 0.1,
-        height: 0,
-    });
-    heroAnimate.from(".lightSpot", {
-        opacity: 0,
-        duration: 1.5,
-        delay: 0.6,
-        height: 0,
-        width: 0,
-    });
-    heroAnimate.from(".tv", {
-        opacity: 0,
-        duration: 0.7,
-        delay: -0.6,
-        height: 0,
-    });
+  const heroAnimate = gsap.timeline()
+  gsap.from(".lamp__line", {
+    opacity: 0,
+    duration: 1,
+    delay: 0,
+    height: 0,
+  })
+  heroAnimate.from(".lamp__triangle", {
+    opacity: 0,
+    duration: 0.6,
+    delay: 0.5,
+    height: 0,
+    width: 0,
+  })
+  heroAnimate.from(".lamp__light", {
+    opacity: 0,
+    duration: 0.3,
+    delay: 0.1,
+    height: 0,
+  })
+  heroAnimate.from(".lightSpot", {
+    opacity: 0,
+    duration: 1.5,
+    delay: 0.6,
+    height: 0,
+    width: 0,
+  })
+  heroAnimate.from(".tv", {
+    opacity: 0,
+    duration: 0.7,
+    delay: -0.6,
+    height: 0,
+  })
 
-    const tvChange = gsap.timeline({ repeat: -1 });
-    tvChange.to('.tv img', {
-        opacity: 1,
-        duration: 0,
-        stagger: 0.07,
-    })
-});
+  const tvChange = gsap.timeline({ repeat: -1 })
+  tvChange.to(".tv img", {
+    opacity: 1,
+    duration: 0,
+    stagger: 0.07,
+  })
+
+  const tv = document.querySelector(".tv");
+//   window.addEventListener("scroll", () => {
+//     let scrollTop = window.scrollY || document.documentElement.scrollTop
+//     let scaleValue = (100 + scrollTop / 10) / 50
+
+//     tv.style.transform = `scale(${scaleValue})`
+//   });
+
+// functon calcMaxScale() {
+//     const rect = 
+// }
+
+})
